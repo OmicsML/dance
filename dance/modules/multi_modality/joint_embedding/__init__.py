a81666e21896589e231280630f58dad0ebcf0524
RemyLau <remylau961@gmail.com>
2022-08-16 08:49:31 -0400

    load main classes at __init__ for multi_modality tasks

--- dance/modules/multi_modality/joint_embedding/__init__.py
+++ dance/modules/multi_modality/joint_embedding/__init__.py
@@ -1,1 +1,12 @@
 # Copyright 2022 DSE lab.  All rights reserved.
+from .dcca import DCCA
+from .jae import JAE
+from .scmogcn import ScMoGCNWrapper
+from .scmvae import scMVAE
+
+__all__ = [
+    "DCCA",
+    "JAE",
+    "ScMoGCNWrapper",
+    "scMVAE",
+]
--- dance/modules/multi_modality/match_modality/__init__.py
+++ dance/modules/multi_modality/match_modality/__init__.py
@@ -1,1 +1,10 @@
 # Copyright 2022 DSE lab.  All rights reserved.
+from .cmae import CMAE
+from .scmm import MMVAE
+from .scmogcn import ScMoGCNWrapper
+
+__all__ = [
+    "CMAE",
+    "MMVAE",
+    "ScMoGCNWrapper",
+]
