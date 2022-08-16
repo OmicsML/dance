2e5ed818df29a92731317dc99d7c249bdfa06727
RemyLau <remylau961@gmail.com>
2022-08-16 08:35:34 -0400

    load main classes at __init__ for single_modality tasks

--- dance/modules/single_modality/clustering/__init__.py
+++ dance/modules/single_modality/clustering/__init__.py
@@ -1,1 +1,14 @@
 # Copyright 2022 DSE lab.  All rights reserved.
+from .graphsc import GraphSC
+from .scdcc import ScDCC
+from .scdeepcluster import ScDeepCluster
+from .scdsc import SCDSC
+from .sctag import SCTAG
+
+__all__ = [
+    "GraphSC",
+    "ScDCC",
+    "ScDeepCluster",
+    "SCDSC",
+    "SCTAG",
+]
--- dance/modules/single_modality/imputation/__init__.py
+++ dance/modules/single_modality/imputation/__init__.py
@@ -1,1 +1,10 @@
 # Copyright 2022 DSE lab.  All rights reserved.
+from .deepimpute import DeepImpute
+from .graphsci import GraphSCI
+from .scgnn import scGNN
+
+__all__ = [
+    "DeepImpute",
+    "GraphSCI",
+    "scGNN",
+]
