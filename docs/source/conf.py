104d09332720ebc852a937299ab8e3555e296723
RemyLau <remylau961@gmail.com>
2022-08-20 12:58:38 -0400

    bump version to 1.0.0-rc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'dance'
 copyright = '2022, DANCE Team'
 author = 'DANCE Team'
-release = '0.0.1'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
