4a45b71d2fcef0b487392c87536e4f9d840abee2
Renming Liu <liurenmi@msu.edu>
2022-10-16 09:11:03 -0400

    create dance logger

--- dance/__init__.py
+++ dance/__init__.py
@@ -1,1 +1,11 @@
 # Copyright 2022 DSE lab.  All rights reserved.
+import logging
+
+from dance import config
+
+logger = logging.getLogger("dance")
+
+__all__ = [
+    "config",
+    "logger",
+]
---
+++ dance/config.py
@@ -0,0 +1,46 @@
+import logging
+import logging.config
+from typing import Union
+
+_logger_config = {
+    "version": 1,
+    "formatters": {
+        "default_formatter": {
+            "format": "[%(levelname)s][%(asctime)s][%(name)s][%(funcName)s] %(message)s",
+        },
+    },
+    "handlers": {
+        "stream_handler": {
+            "class": "logging.StreamHandler",
+            "formatter": "default_formatter",
+            "level": "DEBUG",
+        },
+    },
+    "loggers": {
+        "dance": {
+            "handlers": ["stream_handler"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+logging.config.dictConfig(_logger_config)
+
+
+def change_log_level(name: str = "dance", /, *, level: Union[str, int]):
+    """Change logger level.
+
+    Parameters
+    ----------
+    name: str
+        Name of the logger whose log level is to be updated. Defaut set to global logger.
+    level: Union[str, int]
+        Log level to be updated. Can be either the log level name (str type) or its corresponding code (int type).
+
+    """
+    logging.getLogger(name).setLevel(level)
+
+
+__all__ = [
+    "change_log_level",
+]
