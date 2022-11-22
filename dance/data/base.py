1cb57531c224f45eb0ee26af9e7d122186389086
RemyLau <remylau961@gmail.com>
2022-11-22 16:06:06 -0500

    create abstract BaseData and update Data API

--- dance/data/base.py
+++ dance/data/base.py
@@ -1,49 +1,41 @@
+from abc import ABC, abstractmethod
 from copy import deepcopy
 
 import numpy as np
 import torch
-from anndata import AnnData
 
 from dance import logger
-from dance.typing import CellIdxType, Dict, FeatType, List, Optional, ReturnedFeat, Sequence, Tuple
+from dance.typing import Any, Dict, FeatType, List, Optional, Sequence, Tuple
 
 
-class Data:
+class BaseData(ABC):
     """Base data object."""
 
-    def __init__(self, x: Optional[AnnData] = None, y: Optional[AnnData] = None, train_size: Optional[int] = None,
-                 val_size: int = 0, test_size: int = -1, ensure_cell_aligned: bool = True):
+    def __init__(self, data: Any, train_size: Optional[int] = None, val_size: int = 0, test_size: int = -1):
         """Initialize data object.
 
         Parameters
         ----------
-        x : AnnData
-            Cell features.
-        y : AnnData
-            Cell labels.
+        data
+            Cell data.
         train_size
             Number of cells to be used for training. If not specified, not splits will be generated.
         val_size
             Number of cells to be used for validation. If set to -1, use what's left from training and testing.
         test_size
             Number of cells to be used for testing. If set to -1, used what's left from training and validation.
-        ensure_cell_aligned
-            If set to True, then check for the consistency between the indeices of x and y.
 
         """
-        self._split_idx_dict: Dict[str, Sequence[CellIdxType]] = {}
+        super().__init__()
 
-        self._x = x or AnnData()
-        self._y = y
+        self._data = data
 
-        if ensure_cell_aligned and y:
-            if (x_size := x.shape[0]) != (y_size := y.shape[0]):
-                raise ValueError(f"Mismatched number of samples between x (n={x_size:,}) and y (n={y_size:,}).")
-            elif (num_diff := (x.obs.index != y.obs.index).sum()) > 0:
-                raise IndexError(f"{num_diff:,} out of {x_size:,} entries have mismached indices between x and y. ",
-                                 "Set `ensure_cell_aligned` to False if you do not wish to align x and y.")
-
+        # TODO: move _split_idx_dict into data.uns
+        self._split_idx_dict: Dict[str, Sequence[int]] = {}
         self._setup_splits(train_size, val_size, test_size)
+
+        if "dance_config" not in self._data.uns:
+            self._data.uns["dance_config"] = dict()
 
     def _setup_splits(self, train_size: Optional[int], val_size: int, test_size: int):
         if train_size is None:
@@ -59,7 +51,7 @@ class Data:
             raise ValueError("Only one split can be specified as -1")
 
         # Each size must be bounded between -1 and the data size
-        data_size = self.x.shape[0]
+        data_size = self.num_cells
         for name, size in zip(split_names, split_sizes):
             if size < -1:
                 raise ValueError(f"{name} must be integer no less than -1, got {size!r}")
@@ -74,55 +66,66 @@ class Data:
         split_sizes[split_sizes == -1] = data_size - split_sizes.clip(0).sum()
         logger.debug(f"Split sizes after conversion: {split_sizes.tolist()}")
 
-        all_idx = self.x.obs.index.values
         split_thresholds = split_sizes.cumsum()
         for i, split_name in enumerate(split_names):
             start = split_thresholds[i - 1] if i > 0 else 0
             end = split_thresholds[i]
             if end - start > 0:  # skip empty split
-                self._split_idx_dict[split_name] = all_idx[start:end].tolist()
+                self._split_idx_dict[split_name] = list(range(start, end))
 
-    def __getitem__(self, idx) -> Tuple[AnnData, AnnData]:
-        sliced_x = self.x[idx]
-        sliced_y = self.y[idx]
-        return sliced_x, sliced_y
+    def __getitem__(self, idx: Sequence[int]) -> Any:
+        return self.data[idx]
 
     @property
-    def x(self) -> AnnData:
-        return self._x
+    def data(self):
+        return self._data
 
     @property
-    def y(self) -> Optional[AnnData]:
-        return self._y
+    @abstractmethod
+    def x(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def y(self):
+        raise NotImplementedError
+
+    @property
+    def config(self) -> Dict[str, Any]:
+        return self._data.uns["dance_config"]
+
+    def set_config(self, **kwargs):
+        # TODO: need to have some control about validity of configs
+        self.config.update(kwargs)
 
     @property
     def num_cells(self) -> int:
-        return self.x.shape[0]
+        return self.data.shape[0]
 
     @property
     def num_features(self) -> int:
-        return self.x.shape[1]
+        return self.data.shape[1]
 
     @property
     def cells(self) -> List[str]:
-        return self.x.obs.index.tolist()
+        return self.data.obs.index.tolist()
 
     @property
-    def train_idx(self) -> Sequence[CellIdxType]:
+    def train_idx(self) -> Sequence[int]:
         return self.get_split_idx("train", error_on_miss=False)
 
     @property
-    def val_idx(self) -> Sequence[CellIdxType]:
+    def val_idx(self) -> Sequence[int]:
         return self.get_split_idx("val", error_on_miss=False)
 
     @property
-    def test_idx(self) -> Sequence[CellIdxType]:
+    def test_idx(self) -> Sequence[int]:
         return self.get_split_idx("test", error_on_miss=False)
 
     def copy(self):
         return deepcopy(self)
 
-    def set_split_idx(self, split_name: str, split_idx: Sequence[CellIdxType]):
+    def set_split_idx(self, split_name: str, split_idx: Sequence[int]):
         """Set cell indices for a particular split.
 
         Parameters
@@ -130,7 +133,7 @@ class Data:
         split_name
             Name of the split to set.
         split_idx
-            Indices of the cells to be used in this split.
+            Indices to be used in this split.
 
         """
         self._split_idx_dict[split_name] = split_idx
@@ -153,54 +156,39 @@ class Data:
         else:
             return None
 
-    def _get_feat(self, feat_name: str, split_name: Optional[str], return_type: FeatType = "numpy",
-                  layer: Optional[str] = None, channel: Optional[str] = None):
-        if (layer is not None) and (channel is not None):
-            raise ValueError(f"Cannot specify layer ({layer!r}) and channel ({channel!r}) simmultaneously.")
+    def _get_data(self, name: str, split_name: Optional[str], return_type: FeatType = "numpy", **kwargs):
+        out = getattr(self, name)
 
-        if split_name is None:
-            feat = getattr(self, feat_name)
-        elif split_name in self._split_idx_dict:
+        if hasattr(out, "toarray"):  # convert sparse array to dense numpy array
+            out = out.toarray()
+        elif hasattr(out, "to_numpy"):  # convert dataframe to numpy array
+            out = out.to_numpy()
+
+        if split_name in self._split_idx_dict:
             idx = self.get_split_idx(split_name)
-            feat = getattr(self, feat_name)[idx]
-        else:
+            out = out[idx]
+        elif split_name is not None:
             raise KeyError(f"Unknown split {split_name!r}, available options are {list(self._split_idx_dict)}")
 
-        # Directly return anndata view
-        if return_type == "anndata":
-            return feat
-
-        # Extract features from anndata
-        if layer is not None:
-            feat = feat.layers[layer].X
-        elif channel is not None:
-            feat = feat.obsm[channel]
-        else:
-            feat = feat.X
-
-        try:  # convert sparse array to dense array
-            feat = feat.toarray()
-        except AttributeError:
-            pass
-
         if return_type == "torch":
-            feat = torch.from_numpy(feat)
+            out = torch.from_numpy(out)
         elif return_type != "numpy":
             raise ValueError(f"Unknown return_type {return_type!r}")
 
-        return feat
+        return out
 
-    def get_x(self, split_name: Optional[str] = None, return_type: FeatType = "numpy", layer: Optional[str] = None,
-              channel: Optional[str] = None) -> ReturnedFeat:
+    def get_x(self, split_name: Optional[str] = None, return_type: FeatType = "numpy", **kwargs) -> Any:
         """Retrieve cell features from a particular split."""
-        return self._get_feat("x", split_name, return_type, layer, channel)
+        return self._get_data("x", split_name, return_type, **kwargs)
 
-    def get_y(self, split_name: Optional[str] = None, return_type: FeatType = "numpy") -> ReturnedFeat:
+    def get_y(self, split_name: Optional[str] = None, return_type: FeatType = "numpy", **kwargs) -> Any:
         """Retrieve cell labels from a particular split."""
-        return self._get_feat("y", split_name, return_type)
+        return self._get_data("y", split_name, return_type, **kwargs)
 
-    def get_x_y(self, split_name: Optional[str] = None, return_type: FeatType = "numpy", layer: Optional[str] = None,
-                channel: Optional[str] = None) -> Tuple[ReturnedFeat, ReturnedFeat]:
+    def get_x_y(
+        self, split_name: Optional[str] = None, return_type: FeatType = "numpy", x_kwargs: Dict[str, Any] = dict(),
+        y_kwargs: Dict[str, Any] = dict()
+    ) -> Tuple[Any, Any]:
         """Retrieve cell features and labels from a particular split.
 
         Parameters
@@ -220,21 +208,51 @@ class Data:
         If both `layer` and `channel` are not specified (default), then return the default layer as features.
 
         """
-        x = self.get_x(split_name, return_type, layer, channel)
-        y = self.get_y(split_name, return_type)
+        x = self.get_x(split_name, return_type, **x_kwargs)
+        y = self.get_y(split_name, return_type, **y_kwargs)
         return x, y
 
-    def get_train_data(self, return_type: FeatType = "numpy", layer: Optional[str] = None,
-                       channel: Optional[str] = None) -> Tuple[ReturnedFeat, ReturnedFeat]:
+    def get_train_data(
+        self, return_type: FeatType = "numpy", x_kwargs: Dict[str, Any] = dict(), y_kwargs: Dict[str, Any] = dict()
+    ) -> Tuple[Any, Any]:
         """Retrieve cell features and labels from the 'train' split."""
-        return self.get_x_y("train", return_type, layer, channel)
+        return self.get_x_y("train", return_type, x_kwargs, y_kwargs)
 
-    def get_val_data(self, return_type: FeatType = "numpy", layer: Optional[str] = None,
-                     channel: Optional[str] = None) -> Tuple[ReturnedFeat, ReturnedFeat]:
+    def get_val_data(
+        self, return_type: FeatType = "numpy", x_kwargs: Dict[str, Any] = dict(), y_kwargs: Dict[str, Any] = dict()
+    ) -> Tuple[Any, Any]:
         """Retrieve cell features and labels from the 'val' split."""
-        return self.get_x_y("val", return_type, layer, channel)
+        return self.get_x_y("val", return_type, x_kwargs, y_kwargs)
 
-    def get_test_data(self, return_type: FeatType = "numpy", layer: Optional[str] = None,
-                      channel: Optional[str] = None) -> Tuple[ReturnedFeat, ReturnedFeat]:
+    def get_test_data(
+        self, return_type: FeatType = "numpy", x_kwargs: Dict[str, Any] = dict(), y_kwargs: Dict[str, Any] = dict()
+    ) -> Tuple[Any, Any]:
         """Retrieve cell features and labels from the 'test' split."""
-        return self.get_x_y("test", return_type, layer, channel)
+        return self.get_x_y("test", return_type, x_kwargs, y_kwargs)
+
+    def concat(self, data, merge_splits: bool = True):
+        raise NotImplementedError
+
+
+class Data(BaseData):
+
+    @property
+    def x(self):
+        # TODO: check validity when setting up
+        channel = self.config.get("feature_channel")
+        layer = self.config.get("feature_layer")
+        if (channel is not None) and (layer is not None):
+            raise ValueError(f"Cannot specify feature layer ({layer!r}) and channel ({channel!r}) simmultaneously.")
+        elif channel is not None:
+            return self.data.obsm[channel]
+        elif layer is not None:
+            return self.data.layers[layer].X
+        else:
+            return self.data.X
+
+    @property
+    def y(self):
+        if (channel := self.config.get("label_channel")) is None:
+            raise ValueError("Label channel has not been specified yet.")
+        else:
+            return self.data.obsm[channel]
