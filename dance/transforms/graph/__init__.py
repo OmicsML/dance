7a50d4392de4b3cc0fe9c4ea59ffb0f8830d6cac
Remy <liurenmi@msu.edu>
2022-12-17 18:37:30 -0500

    refactor SpaGCNGraph

--- dance/transforms/graph/__init__.py
+++ dance/transforms/graph/__init__.py
@@ -1,6 +1,9 @@
 from dance.transforms.graph.cell_feature_graph import CellFeatureGraph, PCACellFeatureGraph
+from dance.transforms.graph.spatial_graph import SpaGCNGraph, SpaGCNGraph2D
 
 __all__ = [
     "CellFeatureGraph",
     "PCACellFeatureGraph",
+    "SpaGCNGraph",
+    "SpaGCNGraph2D",
 ]  # yapf: disable
--- dance/transforms/graph_construct.py
+++ dance/transforms/graph_construct.py
@@ -66,49 +66,6 @@ def extract_color(x_pixel=None, y_pixel=None, image=None, beta=49):
     return c3
 
 
-def calculate_adj_matrix(x, y, x_pixel=None, y_pixel=None, image=None, beta=49, alpha=1, histology=True):
-    # x,y,x_pixel, y_pixel are lists
-    if histology:
-        assert (x_pixel is not None) & (x_pixel is not None) & (image is not None)
-        assert (len(x) == len(x_pixel)) & (len(y) == len(y_pixel))
-        print("Calculateing adj matrix using histology image...")
-        # beta to control the range of neighbourhood when calculate grey vale for one spot
-        # alpha to control the color scale
-        beta_half = round(beta / 2)
-        g = []
-        for i in range(len(x_pixel)):
-            max_x = image.shape[0]
-            max_y = image.shape[1]
-            nbs = image[max(0, x_pixel[i] - beta_half):min(max_x, x_pixel[i] + beta_half + 1),
-                        max(0, y_pixel[i] - beta_half):min(max_y, y_pixel[i] + beta_half + 1)]
-            g.append(np.mean(np.mean(nbs, axis=0), axis=0))
-        c0, c1, c2 = [], [], []
-        for i in g:
-            c0.append(i[0])
-            c1.append(i[1])
-            c2.append(i[2])
-        c0 = np.array(c0)
-        c1 = np.array(c1)
-        c2 = np.array(c2)
-        print("Var of c0,c1,c2 = ", np.var(c0), np.var(c1), np.var(c2))
-        c3 = (c0 * np.var(c0) + c1 * np.var(c1) + c2 * np.var(c2)) / (np.var(c0) + np.var(c1) + np.var(c2))
-        c4 = (c3 - np.mean(c3)) / np.std(c3)
-        z_scale = np.max([np.std(x), np.std(y)]) * alpha
-        z = c4 * z_scale
-        z = z.tolist()
-        print("Var of x,y,z = ", np.var(x), np.var(y), np.var(z))
-        X = np.array([x, y, z]).T.astype(np.float32)
-    else:
-        print("Calculateing adj matrix using xy only...")
-        X = np.array([x, y]).T.astype(np.float32)
-    return pairwise_distance(X)
-
-
-def construct_graph(x, y, x_pixel=None, y_pixel=None, image=None, beta=49, alpha=1, histology=True):
-    return calculate_adj_matrix(x, y, x_pixel=x_pixel, y_pixel=y_pixel, image=image, beta=beta, alpha=alpha,
-                                histology=histology)
-
-
 def construct_enhanced_feature_graph(u, v, e, cell_node_features, enhance_graph=None, test=False, **kwargs):
     """Generate a feature-cell graph, enhanced with domain-knowledge (e.g. pathway).
 
