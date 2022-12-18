7106d45ce560167065d72bccbe8a4e5372e067cb
Remy <liurenmi@msu.edu>
2022-12-17 20:43:08 -0500

    remove unused imports and fix lines

--- examples/multi_modality/predict_modality/babel.py
+++ examples/multi_modality/predict_modality/babel.py
@@ -1,20 +1,11 @@
 import argparse
 import logging
-import math
 import os
 import random
 
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import scanpy as sc
-import scipy.spatial
-import skorch.helper
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
-import dance.utils.loss as loss_functions
 from dance.datasets.multimodality import ModalityPredictionDataset
 from dance.modules.multi_modality.predict_modality.babel import BabelWrapper
 from dance.utils import PairedDataset, set_seed
@@ -34,22 +25,12 @@ if __name__ == '__main__':
     parser.add_argument('-seed', '--rnd_seed', default=rndseed, type=int)
     parser.add_argument('-m', '--model_folder', default='./models')
     parser.add_argument("--outdir", "-o", default='./logs', help="Directory to output to")
-    parser.add_argument(
-        "--lossweight",
-        type=float,
-        default=1.,
-        help="Relative loss weight",
-    )
+    parser.add_argument("--lossweight", type=float, default=1., help="Relative loss weight")
     parser.add_argument("--lr", "-l", type=float, default=0.01, help="Learning rate")
     parser.add_argument("--batchsize", "-b", type=int, default=64, help="Batch size")
     parser.add_argument("--hidden", type=int, default=64, help="Hidden dimensions")
     parser.add_argument("--earlystop", type=int, default=20, help="Early stopping after N epochs")
-    parser.add_argument(
-        "--naive",
-        "-n",
-        action="store_true",
-        help="Use a naive model instead of lego model",
-    )
+    parser.add_argument("--naive", "-n", action="store_true", help="Use a naive model instead of lego model")
     parser.add_argument("--resume", action="store_true")
     parser.add_argument("--max_epochs", type=int, default=500)
     args = parser.parse_args()
