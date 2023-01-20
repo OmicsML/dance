4d7d7e68c46cd3f44923f979ad5c6fce52755987
RemyLau <remylau961@gmail.com>
2023-01-19 23:38:52 -0500

    remove model download and loading functionalities since not used

--- dance/modules/single_modality/cell_type_annotation/celltypist.py
+++ dance/modules/single_modality/cell_type_annotation/celltypist.py
@@ -1,4 +1,3 @@
-import json
 import os
 import pathlib
 import pickle
@@ -7,7 +6,6 @@ from typing import Optional, Union
 
 import numpy as np
 import pandas as pd
-import requests
 import scanpy as sc
 from anndata import AnnData
 from matplotlib import pyplot as plt
@@ -25,24 +23,6 @@ models_path = os.path.join(data_path, "models")
 pathlib.Path(models_path).mkdir(parents=True, exist_ok=True)
 
 _samples_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "samples")
-
-
-def _collapse_mean(arr: np.ndarray) -> Union[float, np.ndarray]:
-    """For internal use.
-
-    Average 1D array, or 2D array by row.
-
-    """
-    return np.mean(arr, axis=-1)
-
-
-def _collapse_random(arr: np.ndarray) -> Union[float, np.ndarray]:
-    """For internal use.
-
-    Choose a random number from 1D array, or a random column from 2D array.
-
-    """
-    return np.random.choice(arr, 1)[0] if arr.ndim == 1 else arr[:, np.random.choice(arr.shape[1], 1)[0]]
 
 
 class Model():
@@ -72,35 +52,6 @@ class Model():
         self.classifier = clf
         self.scaler = scaler
         self.description = description
-
-    @staticmethod
-    def load(self, model: Optional[str] = None):
-        """Load the desired model.
-
-        Parameters
-        ----------
-        model: Optional[str]
-            Model name specifying the model you want to load. Default to `'Immune_All_Low.pkl'` if not provided.
-            To see all available models and their descriptions, use :func:`~celltypist.models.models_description`.
-
-        Returns
-        ----------
-        :class:`~celltypist.models.Model`
-            A :class:`~celltypist.models.Model` object.
-
-        """
-        if not model:
-            model = get_default_model()
-        if model in get_all_models():
-            model = get_model_path(model)
-        if not os.path.isfile(model):
-            raise FileNotFoundError(f" No such file: {model}")
-        with open(model, "rb") as fh:
-            try:
-                pkl_obj = pickle.load(fh)
-                return Model(pkl_obj['Model'], pkl_obj['Scaler_'], pkl_obj['description'])
-            except Exception as exception:
-                raise Exception(f" Invalid model: {model}. {exception}")
 
     @property
     def cell_types(self) -> np.ndarray:
@@ -183,188 +134,6 @@ class Model():
         if not only_positive:
             coef_vector = np.abs(coef_vector)
         return self.features[np.argsort(-coef_vector)][:top_n]
-
-
-def get_model_path(file: str) -> str:
-    """Get the full path to a file in the `models` folder.
-
-    Parameters
-    ----------
-    file: str
-        File name as a string.
-        To see all available models and their descriptions, use :func:`~celltypist.models.models_description`.
-
-    Returns
-    ----------
-    str
-        A string of the full path to the desired file.
-
-    """
-    return os.path.join(models_path, f"{file}")
-
-
-def get_default_model() -> str:
-    """Get the default model name.
-
-    Returns
-    ----------
-    str
-        A string showing the default model name (should be `'Immune_All_Low.pkl'`).
-
-    """
-    models_json = get_models_index()
-    default_model = [m["filename"] for m in models_json["models"] if ("default" in m and m["default"])]
-    if not default_model:
-        first_model = models_json["models"][0]["filename"]
-        logger.warn(f" No model marked as 'default', using {first_model}")
-        return first_model
-    if len(default_model) > 1:
-        logger.warn(f" More than one model marked as 'default', using {default_model[0]}")
-    return default_model[0]
-
-
-def get_all_models() -> list:
-    """
-    Get a list of all the available models.
-    Returns
-    ----------
-    list
-        A list of available models.
-    """
-    download_if_required()
-    available_models = []
-    for model_filename in os.listdir(models_path):
-        if model_filename.endswith(".pkl"):
-            model_name = os.path.basename(model_filename)
-            available_models.append(model_name)
-    return available_models
-
-
-def download_if_required() -> None:
-    """Download models if there are none present in the `models` directory."""
-    if len([m for m in os.listdir(models_path) if m.endswith(".pkl")]) == 0:
-        logger.info(" No available models. Downloading...")
-        download_models()
-
-
-def get_models_index(force_update: bool = False) -> dict:
-    """Get the model json object containing the model list.
-
-    Parameters
-    ----------
-    force_update: bool optional
-        If set to `True`, will download the latest model json file from the remote.
-        (Default: `False`)
-
-    Returns
-    ----------
-    dict: dict
-        A dict object converted from the model json file.
-
-    """
-    models_json_path = get_model_path("models.json")
-    if not os.path.exists(models_json_path) or force_update:
-        download_model_index()
-    with open(models_json_path) as f:
-        return json.load(f)
-
-
-def download_model_index(only_model: bool = True) -> None:
-    """Download the `models.json` file from the remote server.
-
-    Parameters
-    ----------
-    only_model: bool
-        If set to `False`, will also download the models in addition to the json file.
-        (Default: `True`)
-
-    Returns
-    ----------
-    No return
-
-    """
-    url = 'https://celltypist.cog.sanger.ac.uk/models/models.json'
-    logger.info(f" Retrieving model list from server {url}")
-    with open(get_model_path("models.json"), "wb") as f:
-        f.write(requests.get(url).content)
-    model_count = len(requests.get(url).json()["models"])
-    logger.info(f" Total models in list: {model_count}")
-    if not only_model:
-        download_models()
-
-
-def download_models(force_update: bool = False, model: Optional[Union[str, list, tuple]] = None) -> None:
-    """Download all the available or selected models.
-
-    Parameters
-    ----------
-    force_update: bool
-        Whether to fetch a latest JSON index for downloading all available or selected models.
-        Set to `True` if you want to parallel the latest celltypist model releases.
-        (Default: `False`)
-    model: Optional[Union[str, list, tuple]]
-        Specific model(s) to download. By default, all available models are downloaded.
-        Set to a specific model name or a list of model names to only download a subset of models.
-        For example, set to `["ModelA.pkl", "ModelB.pkl"]` to only download ModelA and ModelB.
-        To check all available models, use :func:`~celltypist.models.models_description`.
-
-    """
-    models_json = get_models_index(force_update)
-    logger.info(f" Storing models in {models_path}")
-    if model is not None:
-        model_list = {model} if isinstance(model, str) else set(model)
-        models_json["models"] = [m for m in models_json["models"] if m["filename"] in model_list]
-        provided_no = len(model_list)
-        filtered_no = len(models_json["models"])
-        if filtered_no == 0:
-            raise ValueError("No models match the celltypist model repertoire. Please provide valid model names")
-        elif provided_no == filtered_no:
-            logger.info(f" Total models to download: {provided_no}")
-        else:
-            ignored_models = model_list.difference({m["filename"] for m in models_json["models"]})
-            logger.warn(
-                f" Total models to download: {filtered_no}. {len(ignored_models)} not available: {ignored_models}")
-    model_count = len(models_json["models"])
-    for idx, model in enumerate(models_json["models"]):
-        model_path = get_model_path(model["filename"])
-        if os.path.exists(model_path) and not force_update:
-            logger.info(f" Skipping [{idx+1}/{model_count}]: {model['filename']} (file exists)")
-            continue
-        logger.info(f" Downloading model [{idx+1}/{model_count}]: {model['filename']}")
-        try:
-            with open(model_path, "wb") as f:
-                f.write(requests.get(model["url"]).content)
-        except Exception as exception:
-            logger.error(f" {model['filename']} failed {exception}")
-
-
-def models_description(on_the_fly: bool = False) -> pd.DataFrame:
-    """Get the descriptions of all available models.
-
-    Parameters
-    ----------
-    on_the_fly: bool
-        Whether to fetch the model information from downloaded model files.
-        If set to `True`, will fetch the information by loading downloaded models.
-        Default to fetching the information for all available models from the JSON file.
-        (Default: `False`)
-
-    Returns
-    ----------
-    :class:`~pandas.DataFrame`
-        A :class:`~pandas.DataFrame` object with model descriptions.
-
-    """
-    logger.info("Detailed model information can be found at `https://www.celltypist.org/models`")
-    if on_the_fly:
-        filenames = get_all_models()
-        descriptions = [Model.load(filename).description['details'] for filename in filenames]
-    else:
-        models_json = get_models_index()
-        models = models_json["models"]
-        filenames = [model['filename'] for model in models]
-        descriptions = [model['details'] for model in models]
-    return pd.DataFrame({'model': filenames, 'description': descriptions})
 
 
 class AnnotationResult():
@@ -597,9 +366,8 @@ class Classifier():
     ----------
     x: np.ndarray
         Input expression matrix (cell x gene).
-    model: Union[Model,str]
-        A :class:`~celltypist.models.Model` object that wraps the logistic Classifier and the StandardScaler, the
-        path to the desired model file, or the model name.
+    model: Model
+        A :class:`~celltypist.models.Model` object that wraps the logistic Classifier and the StandardScaler.
 
     Attributes
     ----------
@@ -617,8 +385,6 @@ class Classifier():
     """
 
     def __init__(self, x: np.ndarray, model: Model):
-        if isinstance(model, str):
-            model = Model.load(model)
         self.model = model
 
         self.adata = AnnData(x)
