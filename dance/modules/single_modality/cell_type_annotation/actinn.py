c24815c3b1afc73e296361b92bd1bc44c3e701c5
Remy <liurenmi@msu.edu>
2023-01-23 17:13:19 -0500

    move training params for ACTINN to fit function

--- dance/modules/single_modality/cell_type_annotation/actinn.py
+++ dance/modules/single_modality/cell_type_annotation/actinn.py
@@ -10,73 +10,76 @@ import itertools
 from typing import Tuple
 
 import numpy as np
+import scanpy as sc
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+from dance.transforms import AnnDataTransform, Compose, FilterGenesPercentile, SetConfig
+from dance.typing import LogLevel, Optional
 
 
 class ACTINN(nn.Module):
     """The ACTINN cell-type classification model."""
 
     def __init__(
-        self,
-        input_dim: int,
-        output_dim: int,
-        *,
-        batch_size: int = 128,
-        device: str = "cpu",
-        hidden_dims: Tuple[int, ...] = (100, 50, 25),
-        lambd: float = 0.01,
-        lr: float = 0.01,
-        num_epochs: int = 50,
-        print_cost: bool = False,
+            self,
+            *,
+            hidden_dims: Tuple[int, ...] = (100, 50, 25),
+            lambd: float = 0.01,
+            device: str = "cpu",
     ):
         """Initialize the ACTINN model.
 
         Parameters
         ----------
-        input_dim : int
-            Input dimension (number of genes)
-        output_dim : int
-            Output dimension (number of cell types)
         hidden_dims : :obj:`tuple` of int
             Hidden layer dimensions.
-        batch_size : int
-            Training batch size
-        device : str
-            Training device
         lambd : float
             Regularization parameter
-        lr : float
-            Initial learning rate
-        num_epochs : int
-            Number of epochs to run
-        print_cost : bool
-            Print training loss if set to True
+        device : str
+            Training device
 
         """
         super().__init__()
 
         # Save attributes
-        self.batch_size = batch_size
+        self.hidden_dims = hidden_dims
         self.device = device
         self.lambd = lambd
-        self.lr = lr
-        self.num_epochs = num_epochs
-        self.print_cost = print_cost
 
-        # Build MLP
+    def _init_model(self, input_dim: int, output_dim: int):
         self.model = nn.Sequential(
-            nn.Linear(input_dim, hidden_dims[0]),
+            nn.Linear(input_dim, self.hidden_dims[0]),
             nn.ReLU(),
             *itertools.chain.from_iterable(
                 zip(
-                    map(nn.Linear, hidden_dims[:-1], hidden_dims[1:]),
+                    map(nn.Linear, self.hidden_dims[:-1], self.hidden_dims[1:]),
                     itertools.repeat(nn.ReLU()),
                 )),
-            nn.Linear(hidden_dims[-1], output_dim),
-        ).to(device)
+            nn.Linear(self.hidden_dims[-1], output_dim),
+        ).to(self.device)
         print(self.model)
+
+    def preprocess(self, data, /, **kwargs):
+        self.preprocessing_pipeline(**kwargs)(data)
+
+    @staticmethod
+    def preprocessing_pipeline(normalize: bool = True, filter_genes: bool = True, log_level: LogLevel = "INFO"):
+        transforms = []
+
+        if normalize:
+            transforms.append(AnnDataTransform(sc.pp.normalize_total, target_sum=1e4))
+            transforms.append(AnnDataTransform(sc.pp.log1p, base=2))
+
+        if filter_genes:
+            transforms.append(AnnDataTransform(sc.pp.filter_genes, min_cells=1))
+            transforms.append(FilterGenesPercentile(min_val=1, max_val=99, mode="sum"))
+            transforms.append(FilterGenesPercentile(min_val=1, max_val=99, mode="cv"))
+
+        transforms.append(SetConfig({"label_channel": "cell_type"}))
+
+        return Compose(*transforms, log_level=log_level)
 
     def forward(self, x):
         """Forward propagation."""
@@ -136,7 +139,17 @@ class ACTINN(nn.Module):
         slices = [perm[i:i + batch_size] for i in range(0, ns, batch_size)]
         yield from map(lambda slice_: (x[slice_], y[slice_]), slices)
 
-    def fit(self, x_train, y_train, seed=None):
+    def fit(
+        self,
+        x_train,
+        y_train,
+        *,
+        batch_size: int = 128,
+        lr: float = 0.01,
+        num_epochs: int = 50,
+        print_cost: bool = False,
+        seed: Optional[int] = None,
+    ):
         """Fit the classifier.
 
         Parameters
@@ -145,22 +158,32 @@ class ACTINN(nn.Module):
             training data (cells by genes).
         y_train : torch.Tensor
             training labels (cells by cell-types).
+        batch_size : int
+            Training batch size
+        lr : float
+            Initial learning rate
+        num_epochs : int
+            Number of epochs to run
+        print_cost : bool
+            Print training loss if set to True
         seed : int, optional
             Random seed, if set to None, then random.
 
         """
+        input_dim, output_dim = x_train.shape[1], y_train.shape[1]
         x_train = x_train.clone().detach().float().to(self.device)  # cells by genes
         y_train = torch.where(y_train)[1].to(self.device)  # cells
 
         # Initialize weights, optimizer, and scheduler
+        self._init_model(input_dim, output_dim)
         self.initialize_parameters(seed)
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
         lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.95)
 
         # Start training loop
-        for epoch in range(self.num_epochs):
+        for epoch in range(num_epochs):
             epoch_seed = seed if seed is None else seed + epoch
-            batches = self.random_batches(x_train, y_train, self.batch_size, epoch_seed)
+            batches = self.random_batches(x_train, y_train, batch_size, epoch_seed)
 
             tot_cost = tot_size = 0
             for batch_x, batch_y in batches:
@@ -173,7 +196,7 @@ class ACTINN(nn.Module):
                 optimizer.step()
                 lr_scheduler.step()
 
-            if (epoch % 10 == 0) and self.print_cost:
+            if (epoch % 10 == 0) and print_cost:
                 print(f"Epoch: {epoch:>4d} Loss: {tot_cost / tot_size:6.4f}")
 
         print("Parameters have been trained!")
