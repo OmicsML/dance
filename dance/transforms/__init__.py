ea351b5d93e35a44beaa7ae15c1b7bec04ee3f4b
RemyLau <remylau961@gmail.com>
2023-01-23 12:58:20 -0500

    implement Compose transformation, closes #105

--- dance/transforms/__init__.py
+++ dance/transforms/__init__.py
@@ -2,12 +2,13 @@ from dance.transforms import graph
 from dance.transforms.cell_feature import CellPCA, WeightedFeaturePCA
 from dance.transforms.filter import FilterGenesPercentile
 from dance.transforms.interface import AnnDataTransform
-from dance.transforms.misc import SetConfig
+from dance.transforms.misc import Compose, SetConfig
 from dance.transforms.spatial_feature import MorphologyFeature, SMEFeature
 
 __all__ = [
     "AnnDataTransform",
     "CellPCA",
+    "Compose",
     "FilterGenesPercentile",
     "MorphologyFeature",
     "SMEFeature",
--- dance/transforms/misc.py
+++ dance/transforms/misc.py
@@ -1,7 +1,41 @@
 from pprint import pformat
 
 from dance.transforms.base import BaseTransform
-from dance.typing import Any, Dict
+from dance.typing import Any, Dict, Tuple
+
+
+class Compose(BaseTransform):
+    """Compose transformation by combining several transfomration objects."""
+
+    def __init__(self, *transforms: Tuple[BaseTransform, ...], use_master_log_level: bool = True, **kwargs):
+        super().__init__(**kwargs)
+
+        # Check type
+        failed_list = []
+        for transform in transforms:
+            if not isinstance(transform, BaseTransform):
+                failed_list.append(transform)
+        if failed_list:
+            failed_list_str = "\n".join(["\t{i!r}: {type(i)!r}" for i in failed_list])
+            raise TypeError("Expect all transform objects to be inherited from BaseTransform. The following "
+                            f"(n={len(failed_list)}) have incorrect types:\n{failed_list_str}")
+
+        self.transforms = transforms
+
+        # Set log level using master log level from the Compose object
+        if use_master_log_level:
+            for transform in transforms:
+                transform.log_level = self.log_level
+                transform.logger.setLevel(self.log_level)
+
+    def __repr__(self):
+        transform_repr_str = ",\n  ".join(map(repr, self.transforms))
+        return f"Compose(\n  {transform_repr_str},\n)"
+
+    def __call__(self, data):
+        self.logger.info(f"Applying composed transformations:\n{self!r}")
+        for transform in self.transforms:
+            transform(data)
 
 
 class SetConfig(BaseTransform):
