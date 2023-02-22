b12dd2641458c88e52fbc6132477e1ec780fef64
Wenzhuo Tang <tangwen2@msu.edu>
2023-02-22 02:42:00 -0500

    add feature feature graph class

---
+++ dance/transforms/graph/feature_feature_graph.py
@@ -0,0 +1,48 @@
+import dgl
+import dgl.nn as dglnn
+import torch
+from torch.nn import functional as F
+from scipy.sparse import coo_matrix
+
+from dance.transforms.base import BaseTransform
+
+
+class FeatureFeatureGraph(BaseTransform):
+
+    def __init__(self, threshold: float = 0.3, *, normalize_edges: bool = True, **kwargs):
+        super().__init__(**kwargs)
+
+        self.threshold = threshold
+        self.normalize_edges = normalize_edges
+
+    def __call__(self, data):
+        feat = data.get_feature(return_type="torch")
+
+        # Calculate correlation between features
+        corr = torch.corrcoef(feat.T)
+        corr_sub = F.threshold(corr, self.threshold, 0) - F.threshold(-corr, self.threshold, 0)
+        corr_coo = coo_matrix(corr_sub)
+
+        # Initialize graph
+        graph_data = (torch.from_numpy(corr_coo.row).int(), torch.from_numpy(corr_coo.col).int())
+        g = dgl.graph(graph_data, num_nodes=corr.shape[0])
+        g.ndata["feat"] = feat.T
+        g.edata["weight"] = torch.ones(g.num_edges()).float()
+
+        # Normalize edges 
+        if self.normalize_edges:
+            norm = dglnn.EdgeWeightNorm()
+            norm_edge_weight = norm(g, g.edata["weight"])
+            g.edata["weight"] = norm_edge_weight.float()
+        
+        # Get weighted adjacency matrix
+        u, v = graph_data
+        adj = torch.zeros((g.num_nodes(), g.num_nodes()))
+        adj_norm = torch.zeros((g.num_nodes(), g.num_nodes()))
+        adj[u, v] = torch.ones(g.num_edges()).float()
+        adj_norm[u, v] = g.edata['weight']
+
+        data.data.uns[self.out] = g
+        data.data.uns["adj"] = adj
+
+        return data
