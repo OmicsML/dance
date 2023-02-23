1d0811c6fa020006516ff1fbbe781ddfcea667e7
Remy <liurenmi@msu.edu>
2023-02-23 14:52:23 -0500

    update: use auto ct_select

--- examples/spatial/cell_type_deconvo/card.py
+++ examples/spatial/cell_type_deconvo/card.py
@@ -18,9 +18,8 @@ pprint(vars(args))
 # Load dataset
 dataset = CellTypeDeconvoDataset(data_dir=args.datadir, data_id=args.dataset)
 data = dataset.load_data()
-cell_types = data.data.obsm["cell_type_portion"].columns.tolist()
 
-CellTopicProfile(ct_select=cell_types, ct_key="cellType", batch_key=None, split_name="ref", method="mean",
+CellTopicProfile(ct_select="auto", ct_key="cellType", batch_key=None, split_name="ref", method="mean",
                  log_level="INFO")(data)
 FilterGenesMatch(prefixes=["mt-"], case_sensitive=False, log_level="INFO")(data)
 FilterGenesCommon(split_keys=["ref", "test"], log_level="INFO")(data)
@@ -30,7 +29,6 @@ FilterGenesPercentile(min_val=1, max_val=99, mode="rv", log_level="INFO")(data)
 data.set_config(feature_channel=[None, "spatial"], feature_channel_type=["X", "obsm"],
                 label_channel="cell_type_portion")
 (x_count, x_spatial), y = data.get_data(split_name="test", return_type="numpy")
-# TODO: use "auto"/None option for ct_select
 basis = data.get_feature(return_type="default", channel="CellTopicProfile", channel_type="varm")
 
 model = Card(basis)
