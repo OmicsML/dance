56cb0afd146b992c4aea92b3315bb86254ab468f
RemyLau <remylau961@gmail.com>
2023-02-26 14:45:38 -0500

    add new transformation to init

--- dance/transforms/__init__.py
+++ dance/transforms/__init__.py
@@ -1,26 +1,32 @@
 from dance.transforms import graph
 from dance.transforms.cell_feature import CellPCA, WeightedFeaturePCA
-from dance.transforms.filter import FilterGenesCommon, FilterGenesMarker, FilterGenesMatch, FilterGenesPercentile
+from dance.transforms.filter import (FilterCellsScanpy, FilterGenesCommon, FilterGenesMatch, FilterGenesPercentile,
+                                     FilterGenesScanpy)
+from dance.transforms.gene_holdout import GeneHoldout
 from dance.transforms.interface import AnnDataTransform
+from dance.transforms.mask import CellwiseMaskData, MaskData
 from dance.transforms.misc import Compose, RemoveSplit, SaveRaw, SetConfig
 from dance.transforms.normalize import ScaleFeature
 from dance.transforms.pseudo_gen import CellTopicProfile, PseudoMixture
 from dance.transforms.scn_feature import SCNFeature
 from dance.transforms.spatial_feature import MorphologyFeature, SMEFeature
 from dance.transforms.stats import GeneStats
-from dance.transforms.mask import CellwiseMaskData, MaskData
-from dance.transforms.gene_holdout import GeneHoldout
 
 __all__ = [
     "AnnDataTransform",
     "CellPCA",
     "CellTopicProfile",
+    "CellwiseMaskData",
     "Compose",
+    "FilterCellsScanpy",
     "FilterGenesCommon",
     "FilterGenesMarker",
     "FilterGenesMatch",
     "FilterGenesPercentile",
+    "FilterGenesScanpy",
+    "GeneHoldout",
     "GeneStats",
+    "MaskData",
     "MorphologyFeature",
     "PseudoMixture",
     "RemoveSplit",
@@ -31,7 +37,4 @@ __all__ = [
     "SetConfig",
     "WeightedFeaturePCA",
     "graph",
-    "CellwiseMaskData",
-    "MaskData",
-    "GeneHoldout",
 ]  # yapf: disable
