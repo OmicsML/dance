24471435e963b674f1231141271b6bf79d0b265f
Remy <liurenmi@msu.edu>
2023-02-26 21:51:18 -0500

    fix: mask generation logic; support sparse features

--- dance/transforms/mask.py
+++ dance/transforms/mask.py
@@ -1,9 +1,8 @@
 import numpy as np
-import scipy.sparse as sp
 from scipy.stats import expon
 
 from dance.transforms.base import BaseTransform
-from dance.typing import Optional
+from dance.typing import Literal, Optional
 
 
 class CellwiseMaskData(BaseTransform):
@@ -20,46 +19,54 @@ class CellwiseMaskData(BaseTransform):
         Masking rate.
     seed:
         Random seed.
+    Min_gene_counts
+        Minimum number of genes expressed within a below which we do not mask that cell.
 
     """
 
     _DISPLAY_ATTRS = ("distr", "mask_rate", "seed")
 
-    def __init__(self, distr: Optional[str] = "exp", mask_rate: Optional[float] = 0.1, seed: Optional[int] = 1, **kwargs):
+    def __init__(self, distr: Optional[Literal["exp", "uniform"]] = "exp", mask_rate: Optional[float] = 0.1,
+                 seed: Optional[int] = None, min_gene_counts: int = 5, **kwargs):
         super().__init__(**kwargs)
         self.distr = distr
         self.mask_rate = mask_rate
         self.seed = seed
-    
+        self.min_gene_counts = min_gene_counts
+
     def _get_probs(self, vec):
-        return {
-            "exp": expon.pdf(vec, 0, 20),
-            "uniform": np.tile([1. / len(vec)], len(vec)),
-        }.get(self.distr)
+        if self.distr == "exp":
+            prob = expon.pdf(vec, 0, 20)
+        elif self.distr == "uniform":
+            prob = np.ones(len(vec))
+        else:
+            raise ValueError(f"Unknown distribution function option {self.distr!r}, "
+                             "available options are: 'exp', 'uniform'")
+        return prob / prob.sum()
 
     def __call__(self, data):
         rng = np.random.default_rng(self.seed)
-        feat = data.get_feature(return_type="numpy")
-        trainMask = np.ones_like(feat).astype(bool)
+        feat = data.get_feature(return_type="default")
+        train_mask = np.ones(feat.shape, dtype=bool)
+
         for c in range(feat.shape[0]):
-            cells_c = feat[c, :]
             # Retrieve indices of positive values
-            ind_pos = np.arange(feat.shape[1])[cells_c > 0]
-            cells_c_pos = cells_c[ind_pos]
+            ind_pos = np.nonzero(feat[c])[-1]
+            cells_c_pos = feat[c, ind_pos]
 
             # Get masking probability of each value
-            if cells_c_pos.size > 5:
-                probs = self._get_probs(cells_c_pos)
-                n_masked = int(np.floor(len(cells_c_pos) * self.mask_rate))
+            if cells_c_pos.size > self.min_gene_counts:
+                prob = self._get_probs(cells_c_pos)
+                n_masked = int(np.floor(cells_c_pos.size * self.mask_rate))
                 if n_masked >= cells_c_pos.size:
-                    print("Warning: too many cells masked for gene {} ({}/{})".format(c, n_masked, cells_c_pos.size))
-                    n_masked = 1 + int(0.5 * cells_c_pos.size)
+                    self.logger.warning(f"Too many genes masked for cell {c} ({n_masked}/{cells_c_pos.size})")
+                    n_masked = 1 + int(np.floor(0.5 * cells_c_pos.size))
 
-                masked_idx = rng.choice(cells_c_pos.size, n_masked, p=probs / probs.sum(), replace=False)
-                trainMask[c, ind_pos[sorted(masked_idx)]] = False
-        
-        data.data.layers["train_mask"] = trainMask
-        data.data.layers["valid_mask"] = ~trainMask
+                masked_idx = rng.choice(cells_c_pos.size, n_masked, p=prob, replace=False)
+                train_mask[c, ind_pos[masked_idx]] = False
+
+        data.data.layers["train_mask"] = train_mask
+        data.data.layers["valid_mask"] = ~train_mask
 
         return data
 
@@ -80,11 +87,11 @@ class MaskData(BaseTransform):
 
     _DISPLAY_ATTRS = ("mask_rate", "seed")
 
-    def __init__(self, mask_rate: Optional[float] = 0.1, seed: Optional[int] = 1, **kwargs):
+    def __init__(self, mask_rate: Optional[float] = 0.1, seed: Optional[int] = None, **kwargs):
         super().__init__(**kwargs)
         self.mask_rate = mask_rate
         self.seed = seed
-    
+
     def _get_probs(self, vec):
         return {
             "exp": expon.pdf(vec, 0, 20),
@@ -94,23 +101,16 @@ class MaskData(BaseTransform):
     def __call__(self, data):
         rng = np.random.default_rng(self.seed)
         feat = data.get_feature(return_type="default")
-        trainMask = np.ones_like(feat).astype(bool)
-
+        train_mask = np.ones(feat.shape, dtype=bool)
         row, col = np.nonzero(feat)
-        feat_pos = feat[row, col]
-        
+        num_nonzero = len(row)
+
         # Randomly mask positive counts according to masking rate.
-        num_valid = int(np.floor(len(row) * self.mask_rate))
-        num_train = len(row) - num_valid
-        all_features_idx = np.arange(len(feat_pos))
-        rng.shuffle(all_features_idx)
-        train_data_idx = all_features_idx[:num_train]
-        valid_data_idx = all_features_idx[num_train:(num_train+num_valid)]
-        
-        for i in train_data_idx:
-            trainMask[row[i], col[i]] = False
-        
-        data.data.layers["train_mask"] = trainMask
-        data.data.layers["valid_mask"] = ~trainMask
+        num_train = num_nonzero - int(np.floor(num_nonzero * self.mask_rate))
+        mask_idx = rng.choice(num_nonzero, size=num_train, replace=False)
+        train_mask[row[mask_idx], col[mask_idx]] = False
+
+        data.data.layers["train_mask"] = train_mask
+        data.data.layers["valid_mask"] = ~train_mask
 
         return data
