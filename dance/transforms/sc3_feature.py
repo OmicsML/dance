62b82b3982f89c67126f961e86faec7615a522a6
xingzhongyu <57212168+xingzhongyu@users.noreply.github.com>
2023-12-04 16:40:13 +0800

    Update sc3_feature.py

--- dance/transforms/sc3_feature.py
+++ dance/transforms/sc3_feature.py
@@ -22,23 +22,28 @@ def normalized_laplacian(adj_matrix):
 
 
 class SC3Feature(BaseTransform):
-
-    def __init__(self, threshold: float = 0.3, *, normalize_edges: bool = True, n_cluster: int = 3, **kwargs):
+"""
+SC3 computes a consensus matrix using the cluster-based similarity partitioning algorithm (CSPA)33. For each individual clustering result,a binary similarity matrix is constructed from the corresponding cell labels: if two cells belong to
+the same cluster, their similarity is 1; otherwise, the similarity is 0. A consensus matrix is calculated by averaging all similarity matrices of individual clusterings. To reduce computational time, if the length of the d range 
+is more than 15, a random subset of 15 values selected uniformly from the d range is used.
+For more details,see the [SC3: consensus clustering of single-cell RNA-seq data](https://www.nature.com/articles/nmeth.4236#Sec2)
+n_cluster:the number of clusters in kmeans
+d:number of cells selected
+"""
+    def __init__(self, n_cluster: int = 3,d=None **kwargs):
         super().__init__(**kwargs)
-        self.threshold = threshold
-        self.normalize_edges = normalize_edges
         self.n_cluster = n_cluster
         self.choices = None
-
-    def __call__(self, data, d=None):
+        self.d=d
+    def __call__(self, data):
         feat = data.get_feature(return_type="numpy")
         num_cells = feat.shape[0]
-        if d is None:
-            d = math.ceil(num_cells * 0.07) - math.floor(num_cells * 0.04)
-        if d > 15:
-            self.choices = sorted(np.random.choice(range(d), 15, replace=False))
+        if self.d is None:
+            self.d = math.ceil(num_cells * 0.07) - math.floor(num_cells * 0.04)
+        if self.d > 15:
+            self.choices = sorted(np.random.choice(range(self.d), 15, replace=False))
         else:
-            self.choices = list(range(d))
+            self.choices = list(range(self.d))
         y_len = feat.shape[0]
         sc3_mats = []
         for i in range(3):
