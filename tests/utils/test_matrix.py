17f36946455f9959f7648438c2c162b6a1ab9366
RemyLau <remylau961@gmail.com>
2023-12-05 12:04:22 -0500

    test pairwise distance computation

--- tests/utils/test_matrix.py
+++ tests/utils/test_matrix.py
@@ -1,4 +1,7 @@
+import itertools
+
 import numpy as np
+import scipy.stats
 
 from dance.utils import matrix
 
@@ -24,3 +27,39 @@ def test_normalize(subtests):
 
         mat_norm1 = (mat / np.sqrt((mat**2).sum(1, keepdims=True))).tolist()
         assert matrix.normalize(mat, mode="l2", axis=1).tolist() == mat_norm1
+
+
+def test_pairwise_distance(subtests):
+    mat = np.array([
+        [0, 1, 2],
+        [2, 2, 4],
+        [5, 3, 5],
+        [3, 2, 1],
+        [5, 6, 3],
+    ], dtype=np.float32)  # yapf: disable
+
+    def compute_pairwise(ary, func):
+        size = ary.shape[0]
+        out = np.zeros((size, size), dtype=np.float32)
+        for i, j in itertools.product(range(size), range(size)):
+            if i <= j:
+                out[i, j] = out[j, i] = func(ary[i], ary[j])
+        return out
+
+    with subtests.test("euclidean"):
+        ans = compute_pairwise(mat, scipy.spatial.distance.euclidean)
+        res = matrix.pairwise_distance(mat, 0)
+        print(f"\n\n{ans=}\n{res=}")
+        assert np.allclose(ans, res)
+
+    with subtests.test("pearson"):
+        ans = compute_pairwise(mat, lambda x, y: 1 - scipy.stats.pearsonr(x, y)[0])
+        res = matrix.pairwise_distance(mat, 1)
+        print(f"\n\n{ans=}\n{res=}")
+        assert np.allclose(ans, res)
+
+    with subtests.test("spearman"):
+        ans = compute_pairwise(mat, lambda x, y: 1 - scipy.stats.spearmanr(x, y)[0])
+        res = matrix.pairwise_distance(mat, 2)
+        print(f"\n\n{ans=}\n{res=}")
+        assert np.allclose(ans, res)
