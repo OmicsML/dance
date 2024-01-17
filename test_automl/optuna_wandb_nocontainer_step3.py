40a7c01bceef780b053a39eea79ac375c0f52d5c
xzy <xzy12386@163.com>
2024-01-17 22:17:26 +0800

    Delete useless comments

--- test_automl/optuna_wandb_nocontainer_step3.py
+++ test_automl/optuna_wandb_nocontainer_step3.py
@@ -1,15 +1,13 @@
-import json
-import os
 import sys
 
 import numpy as np
 import optuna
 import scanpy as sc
 import torch
-import wandb
 from optuna.integration.wandb import WeightsAndBiasesCallback
 from test_automl_step2 import fun2code_dict
 
+import wandb
 from dance.datasets.singlemodality import CellTypeAnnotationDataset
 from dance.modules.single_modality.cell_type_annotation.actinn import ACTINN
 from dance.transforms.cell_feature import CellPCA, CellSVD, WeightedFeaturePCA
@@ -22,39 +20,20 @@ from dance.utils import set_seed
 fun_list = ["log1p", "filter_gene_by_count"]
 wandb_kwargs = {"project": "my-project"}
 wandbc = WeightsAndBiasesCallback(wandb_kwargs=wandb_kwargs, as_multirun=True)
-# import inspect
-
-# path= os.path.dirname(inspect.getfile(lambda: None))
-# path = os.path.join(path,"fun_list.json") # 拼接文件的路径
-# with open(path,"r") as file:
-#     loaded_data = json.load(file)
-# fun_list = loaded_data["fun_list"]
-
-# print(path)
-# from fun_list import fun_list
-# from dance.transforms.cell_feature import CellPCA,CellSVD,WeightedFeaturePCA
-# from dance.transforms.filter import FilterGenesPercentile, FilterGenesRegression
-# from dance.transforms.interface import AnnDataTransform
-# from dance.transforms.normalize import ScTransformR, ScaleFeature
 
 
 def cell_pca(trial: optuna.Trial):
     method_name = str(sys._getframe().f_code.co_name) + "_"
-    # return {method_name + "n_components": trial.suggest_int(method_name + "n_components", 200, 5000)}
     return CellPCA(n_components=trial.suggest_int(method_name + "n_components", 200, 5000))
 
 
 def cell_weighted_pca(trial: optuna.Trial):
     method_name = str(sys._getframe().f_code.co_name) + "_"
-    # return {method_name + "n_components": trial.suggest_int(method_name + "n_components", 200, 5000)}
-
     return WeightedFeaturePCA(n_components=trial.suggest_int(method_name + "n_components", 200, 5000))
 
 
 def cell_svd(trial: optuna.Trial):
     method_name = str(sys._getframe().f_code.co_name) + "_"
-    # return {method_name + "n_components": trial.suggest_int(method_name + "n_components", 200, 5000)}
-
     return CellSVD(n_components=trial.suggest_int(method_name + "n_components", 200, 5000))
 
 
@@ -63,10 +42,6 @@ def Filter_gene_by_regress_score(trial: optuna.Trial):
     return FilterGenesRegression(
         method=trial.suggest_categorical(method_name + "method", ["enclasc", "seurat3", "scmap"]),
         num_genes=trial.suggest_int(method_name + "num_genes", 5000, 6000))
-    # return {
-    #     method_name + "method": trial.suggest_categorical(method_name + "method", ["enclasc", "seurat3", "scmap"]),
-    #     method_name + "num_genes": trial.suggest_int(method_name + "num_genes", 5000, 6000)
-    # }
 
 
 def highly_variable_genes(trial: optuna.Trial):
@@ -78,14 +53,6 @@ def highly_variable_genes(trial: optuna.Trial):
                             span=trial.suggest_float(method_name + "span", 0.2,
                                                      1.0), n_bins=trial.suggest_int(method_name + "n_bins", 10, 30),
                             flavor=trial.suggest_categorical(method_name + "flavor", ['seurat', 'cell_ranger']))
-    # return {
-    #     method_name + "min_mean": trial.suggest_float(method_name + "min_mean", 0.0025, 0.03),
-    #     method_name + "max_mean": trial.suggest_float(method_name + "min_mean", 1.5, 4.5),
-    #     method_name + "min_disp": trial.suggest_float(method_name + "min_disp", 0.25, 0.75),
-    #     method_name + "span": trial.suggest_float(method_name + "span", 0.2, 1.0),
-    #     method_name + "n_bins": trial.suggest_int(method_name + "n_bins", 10, 30),
-    #     method_name + "flavor": trial.suggest_categorical(method_name + "flavor", ['seurat', 'cell_ranger'])
-    # }
 
 
 def filter_gene_by_percentile(trial: optuna.Trial):
@@ -93,11 +60,6 @@ def filter_gene_by_percentile(trial: optuna.Trial):
     return FilterGenesPercentile(min_val=trial.suggest_int(method_name + "min_val", 1, 10),
                                  max_val=trial.suggest_int(method_name + "max_val", 90, 99),
                                  mode=trial.suggest_categorical(method_name + "mode", ["sum", "var", "cv", "rv"]))
-    # return {
-    #     method_name + "min_val": trial.suggest_int(method_name + "min_val", 1, 10),
-    #     method_name + "max_val": trial.suggest_int(method_name + "max_val", 90, 99),
-    #     method_name + "mode": trial.suggest_categorical(method_name + "mode", ["sum", "var", "cv", "rv"])
-    # }
 
 
 def filter_gene_by_count(trial: optuna.Trial):
@@ -112,19 +74,15 @@ def filter_gene_by_count(trial: optuna.Trial):
     if method == "max_cells":
         num = trial.suggest_int(method_name + "num", 500, 1000)
     return AnnDataTransform(sc.pp.filter_genes, **{method: num})
-    # return {method_name + "method": method, method_name + "num": num}
 
 
 def log1p(trial: optuna.Trial):
     method_name = str(sys._getframe().f_code.co_name) + "_"
     return AnnDataTransform(sc.pp.log1p, base=trial.suggest_int(method_name + "base", 2, 10))
-    # return {method_name + "base": trial.suggest_int(method_name + "base", 2, 10)}
 
 
 def scTransform(trial: optuna.Trial):
     method_name = str(sys._getframe().f_code.co_name) + "_"
-    # return {method_name + "min_cells": trial.suggest_int(method_name + "min_cells", 1, 10)}
-
     return ScTransformR(min_cells=trial.suggest_int(method_name + "min_cells", 1, 10))
 
 
@@ -132,10 +90,6 @@ def scaleFeature(trial: optuna.Trial):  #eps未优化
     method_name = str(sys._getframe().f_code.co_name) + "_"
     return ScaleFeature(mode=trial.suggest_categorical(method_name +
                                                        "mode", ["normalize", "standardize", "minmax", "l2"]))
-    # return {
-    #     method_name + "mode": trial.suggest_categorical(method_name + "mode",
-    #                                                     ["normalize", "standardize", "minmax", "l2"])
-    # }
 
 
 def normalize_total(trial: optuna.Trial):
@@ -147,44 +101,21 @@ def normalize_total(trial: optuna.Trial):
         return AnnDataTransform(sc.pp.normalize_total,
                                 target_sum=trial.suggest_categorical(method_name + "target_sum", [1e4, 1e5, 1e6]),
                                 exclude_highly_expressed=exclude_highly_expressed, max_fraction=max_fraction)
-        # return {
-        #     method_name + "exclude_highly_expressed":
-        #     trial.suggest_categorical(method_name + "exclude_highly_expressed", [False, True]),
-        #     method_name + "max_fraction":
-        #     max_fraction
-        # }
     else:
         return AnnDataTransform(sc.pp.normalize_total,
                                 target_sum=trial.suggest_categorical(method_name + "target_sum", [1e4, 1e5, 1e6]),
                                 exclude_highly_expressed=exclude_highly_expressed, max_fraction=max_fraction)
-        # return {
-        #     method_name + "exclude_highly_expressed":
-        #     trial.suggest_categorical(method_name + "exclude_highly_expressed", [False, True])
-        # }
 
-
-# 从 JSON 文件中读取数据
-
-# parameter_config = None
 
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 
 @wandbc.track_in_wandb()
 def objective(trial):
-    # method_name=str(sys._getframe().f_code.co_name)+"_"
-    # exclude_highly_expressed=trial.suggest_categorical(method_name+"exclude_highly_expressed",[False,True])
-    # if exclude_highly_expressed:
-    #     max_fraction=trial.suggest_float(method_name+"max_fraction",0.04,0.1)
-    # print(exclude_highly_expressed)
-    # if exclude_highly_expressed:
-    #     print(max_fraction)
-    # global parameter_config
-    # parameter_config = {}
+
     transforms = []
     for f_str in fun_list:
         fun_i = eval(f_str)
-        # parameter_config.update(fun_i(trial))
         transforms.append(fun_i(trial))
     parameters_dict = {
         'batch_size': 128,
@@ -225,9 +156,6 @@ def objective(trial):
                   num_epochs=parameter_config.get('num_epochs'), batch_size=parameter_config.get('batch_size'),
                   print_cost=False)
         scores.append(score := model.score(x_test, y_test))
-    #     print(f"{score=:.4f}")
-    # print(f"ACTINN {species} {tissue} {test_dataset}:")
-    # print(f"{scores}\n{np.mean(scores):.5f} +/- {np.std(scores):.5f}")
     wandb.log({"scores": np.mean(scores)})
     return np.mean(scores)
 
