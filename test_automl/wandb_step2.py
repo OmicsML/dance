2c703cea86d31efd1a3677c9ff13f576cd02a396
xzy <xzy12386@163.com>
2024-01-19 09:09:04 +0800

    global pipline2fun_dict

--- test_automl/wandb_step2.py
+++ test_automl/wandb_step2.py
@@ -1,8 +1,8 @@
 from itertools import combinations
 
 import scanpy as sc
-import wandb
 
+import wandb
 from dance.transforms.cell_feature import CellPCA, CellSVD, WeightedFeaturePCA
 from dance.transforms.filter import FilterGenesPercentile, FilterGenesRegression
 from dance.transforms.interface import AnnDataTransform
@@ -22,23 +22,22 @@ fun2code_dict = {
     "cell_weighted_pca": WeightedFeaturePCA(split_name="train"),
     "cell_pca": CellPCA()
 }
+pipline2fun_dict = {
+    "normalize": {
+        "values": ["normalize_total", "log1p", "scaleFeature", "scTransform"]
+    },
+    "gene_filter": {
+        "values":
+        ["filter_gene_by_count", "filter_gene_by_percentile", "highly_variable_genes", "Filter_gene_by_regress_score"]
+    },
+    "gene_dim_reduction": {
+        "values": ["cell_svd", "cell_weighted_pca", "cell_pca"]
+    }
+}
 
 
 def getSweepId(selected_keys=["normalize", "gene_filter", "gene_dim_reduction"]):
-    pipline2fun_dict = {
-        "normalize": {
-            "values": ["normalize_total", "log1p", "scaleFeature", "scTransform"]
-        },
-        "gene_filter": {
-            "values": [
-                "filter_gene_by_count", "filter_gene_by_percentile", "highly_variable_genes",
-                "Filter_gene_by_regress_score"
-            ]
-        },
-        "gene_dim_reduction": {
-            "values": ["cell_svd", "cell_weighted_pca", "cell_pca"]
-        }
-    }
+    global pipline2fun_dict
     pipline2fun_dict = {key: pipline2fun_dict[key] for key in selected_keys}
     count = 1
     for pipline_key, pipline_values in pipline2fun_dict.items():
