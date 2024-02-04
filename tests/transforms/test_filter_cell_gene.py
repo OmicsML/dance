4657a0e3af3333619415999772185e341ab6ce56
Remy <liurenmi@msu.edu>
2024-02-04 10:09:36 -0500

    format edit

--- tests/transforms/test_filter_cell_gene.py
+++ tests/transforms/test_filter_cell_gene.py
@@ -3,9 +3,13 @@ import pytest
 from anndata import AnnData
 
 from dance.data import Data
-from dance.transforms import (FilterCellsScanpyOrder, FilterGenesScanpyOrder,
-                              HighlyVariableGenesLogarithmizedByMeanAndDisp, HighlyVariableGenesLogarithmizedByTopGenes,
-                              HighlyVariableGenesRawCount)
+from dance.transforms import (
+    FilterCellsScanpyOrder,
+    FilterGenesScanpyOrder,
+    HighlyVariableGenesLogarithmizedByMeanAndDisp,
+    HighlyVariableGenesLogarithmizedByTopGenes,
+    HighlyVariableGenesRawCount,
+)
 
 SEED = 123
 
@@ -18,9 +22,16 @@ def toy_data():
     return adata, data
 
 
-@pytest.mark.parametrize('order',
-                         [['min_counts', 'min_cells', 'max_counts', 'max_cells'],
-                          ['max_counts', 'min_cells', 'min_counts'], ['min_cells', 'min_counts'], ['min_counts'], []])
+@pytest.mark.parametrize(
+    'order',
+    [
+        ['min_counts', 'min_cells', 'max_counts', 'max_cells'],
+        ['max_counts', 'min_cells', 'min_counts'],
+        ['min_cells', 'min_counts'],
+        ['min_counts'],
+        [],
+    ],
+)
 def test_filter_genes_scanpy_order(toy_data, order):
     adata, data = toy_data
     filterGenesScanpy = FilterGenesScanpyOrder(order=order, min_counts=1, min_cells=1, max_counts=3000, max_cells=20)
@@ -29,9 +40,16 @@ def test_filter_genes_scanpy_order(toy_data, order):
     assert X.shape[0] == data.shape[0]
 
 
-@pytest.mark.parametrize('order',
-                         [['min_counts', 'min_genes', 'max_counts', 'max_genes'],
-                          ['max_counts', 'min_genes', 'min_counts'], ['min_genes', 'min_counts'], ['min_counts'], []])
+@pytest.mark.parametrize(
+    'order',
+    [
+        ['min_counts', 'min_genes', 'max_counts', 'max_genes'],
+        ['max_counts', 'min_genes', 'min_counts'],
+        ['min_genes', 'min_counts'],
+        ['min_counts'],
+        [],
+    ],
+)
 def test_filter_cells_scanpy_order(toy_data, order):
     adata, data = toy_data
     filterCellsScanpy = FilterCellsScanpyOrder(order=order, min_counts=1, min_genes=1, max_counts=3000, max_genes=20)
@@ -43,14 +61,17 @@ def test_filter_cells_scanpy_order(toy_data, order):
 def test_hvg(subtests):
     adata = AnnData(X=np.log1p(np.array(np.arange(1500)).reshape(50, 30)))
     data = Data(adata.copy())
+
     with subtests.test("HighlyVariableGenesRawCount"):
         hvg = HighlyVariableGenesRawCount(n_top_genes=20)
         hvg(data)
         assert adata.X.shape[0] == data.data.X.shape[0]
+
     with subtests.test("HighlyVariableGenesLogarithmizedByTopGenes"):
         hvg = HighlyVariableGenesLogarithmizedByTopGenes(n_top_genes=20)
         hvg(data)
         assert adata.X.shape[0] == data.data.X.shape[0]
+
     with subtests.test("HighlyVariableGenesLogarithmizedByMeanAndDisp"):
         hvg = HighlyVariableGenesLogarithmizedByMeanAndDisp()
         hvg(data)
--- tests/transforms/test_normalize.py
+++ tests/transforms/test_normalize.py
@@ -8,10 +8,12 @@ from dance.transforms import Log1P, NormalizeTotal
 def test_normalize_total(subtests):
     adata = AnnData(X=np.array([[1, 1, 1], [1, 1, 1], [3, 0, 0]]))
     data = Data(adata.copy())
+
     with subtests.test("max_fraction is less than 1.0"):
         normalizeTotal = NormalizeTotal(max_fraction=0.99, target_sum=30)
         normalizeTotal(data)
         assert (data.data.X == np.array([[15.0, 15.0, 15.0], [15.0, 15.0, 15.0], [90.0, 0.0, 0.0]])).all()
+
     with subtests.test("max_fraction is equal to 1.0"):
         normalizeTotal = NormalizeTotal(max_fraction=1.0, target_sum=30)
         normalizeTotal(data)
@@ -21,6 +23,8 @@ def test_normalize_total(subtests):
 def test_log1p():
     adata = AnnData(X=np.array([[1, 1, 1], [1, 1, 1], [3, 0, 0]]))
     data = Data(adata.copy())
+
     log1p = Log1P()
     log1p(data)
+
     assert data.data.X.shape[0] == adata.X.shape[0]
