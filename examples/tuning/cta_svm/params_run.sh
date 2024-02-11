e2001095fb1cbe35c92d8615f559121756082889
xzy <xzy12386@163.com>
2024-02-11 18:33:01 +0800

    update file location

--- examples/tuning/cta_svm/params_run.sh
+++ examples/tuning/cta_svm/params_run.sh
@@ -1,3 +1,3 @@
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/1st_test_acc_  --tune_mode=params --count=10 > temp_data/8.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/2nd_test_acc_  --tune_mode=params --count=10 > temp_data/9.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/3rd_test_acc_  --tune_mode=params --count=10 > temp_data/10.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/1st_test_acc_  --tune_mode=params --count=10 > temp_data/8.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/2nd_test_acc_  --tune_mode=params --count=10 > temp_data/9.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/3rd_test_acc_  --tune_mode=params --count=10 > temp_data/10.log 2>&1 &
