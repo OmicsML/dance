3b0e68974dd1bcbe9c2c5e1f6a5d503c533ff4fe
xzy <xzy12386@163.com>
2024-02-11 16:16:15 +0800

    update step3

--- examples/tuning/cta_svm/params_run.sh
+++ examples/tuning/cta_svm/params_run.sh
@@ -1,3 +1,3 @@
-# python examples/tuning/cta_svm/main.py --config_dir=config_yamls/subset_0_  --tune_mode=params --count=10 > temp_data/8.log 2>&1 &
-# python examples/tuning/cta_svm/main.py --config_dir=config_yamls/subset_1_  --tune_mode=params --count=10 > temp_data/9.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/subset_2_  --tune_mode=params --count=10 > temp_data/10.log 2>&1
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/subset_0_  --tune_mode=params --count=10 > temp_data/8.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/subset_1_  --tune_mode=params --count=10 > temp_data/9.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/subset_2_  --tune_mode=params --count=10 > temp_data/10.log 2>&1 &
