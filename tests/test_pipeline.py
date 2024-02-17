d85d262f2e0428d140c11a89dd2ae6a98e2dd800
xzy <xzy12386@163.com>
2024-02-17 09:20:21 +0800

    update test

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1032,11 +1032,11 @@ def test_pipeline_subset():
     assert repr(command_str) == repr("""#!/bin/bash
 log_dir=temp_data
 mkdir -p ${log_dir}
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_0_ --count=4 > temp_data/0.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_1_ --count=7 > temp_data/1.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_2_ --count=4 > temp_data/2.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_3_ --count=28 > temp_data/3.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_4_ --count=16 > temp_data/4.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_5_ --count=28 > temp_data/5.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_6_ --count=112 > temp_data/6.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_0_ --count=5 > temp_data/0.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_1_ --count=8 > temp_data/1.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_2_ --count=5 > temp_data/2.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_3_ --count=40 > temp_data/3.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_4_ --count=25 > temp_data/4.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_5_ --count=40 > temp_data/5.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/pipeline/subset_6_ --count=200 > temp_data/6.log 2>&1 &
 """)
