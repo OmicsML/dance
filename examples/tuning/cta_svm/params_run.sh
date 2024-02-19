338d0942296f2c8843cb6076310652792e9a52fc
xingzhongyu <57212168+xingzhongyu@users.noreply.github.com>
2024-02-19 22:11:58 +0800

    Minor changes

--- examples/tuning/cta_svm/params_run.sh
+++ examples/tuning/cta_svm/params_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 log_dir="temp_data"
 mkdir -p "${log_dir}"
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/1st_test_acc_  --tune_mode=params --count=10 > ${log_dir}/8.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/2nd_test_acc_  --tune_mode=params --count=10 > ${log_dir}/9.log 2>&1 &
-python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/3rd_test_acc_  --tune_mode=params --count=10 > ${log_dir}/10.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/1_test_acc_  --tune_mode=params --count=10 > ${log_dir}/8.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/2_test_acc_  --tune_mode=params --count=10 > ${log_dir}/9.log 2>&1 &
+python examples/tuning/cta_svm/main.py --config_dir=config_yamls/params/3_test_acc_  --tune_mode=params --count=10 > ${log_dir}/10.log 2>&1 &
