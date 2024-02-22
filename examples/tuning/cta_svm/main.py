d3ef7d0f8f1abc29153c68721a7b179e72eff0e3
xzy <xzy12386@163.com>
2024-02-22 20:54:26 +0800

    update main

--- examples/tuning/cta_svm/main.py
+++ examples/tuning/cta_svm/main.py
@@ -1,15 +1,12 @@
 import argparse
-import os
 import pprint
-import subprocess
 import sys
-from functools import partial
 from pathlib import Path
 from typing import get_args
 
-import wandb
 from sklearn.random_projection import GaussianRandomProjection
 
+import wandb
 from dance import logger
 from dance.datasets.singlemodality import CellTypeAnnotationDataset
 from dance.modules.single_modality.cell_type_annotation.svm import SVM
