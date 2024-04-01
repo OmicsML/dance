af406465560541e91801c620454423397a416e45
xzy <xzy12386@163.com>
2024-04-01 22:50:40 +0800

    minor changes

--- examples/tuning/get_important_pattern.py
+++ examples/tuning/get_important_pattern.py
@@ -10,9 +10,11 @@ import scikit_posthocs as sp
 import seaborn as sns
 from mlxtend.frequent_patterns import apriori
 from mlxtend.preprocessing import TransactionEncoder
+from networkx import parse_adjlist
 from scipy import stats
 
 metric_name = "acc"
+ascending = False
 
 
 def get_important_pattern(test_accs, vis=True, alpha=0.8, title=""):
@@ -27,7 +29,7 @@ def get_important_pattern(test_accs, vis=True, alpha=0.8, title=""):
     if p_value < alpha:
         data = test_accs
         p_values_matrix = sp.posthoc_dunn(a=data)
-        sorted_indices = np.argsort(medians)
+        sorted_indices = np.argsort(np.argsort(medians * -1 if ascending else medians))
         ranks = {
             index: {
                 "rank": rank,
@@ -56,7 +58,8 @@ def get_important_pattern(test_accs, vis=True, alpha=0.8, title=""):
                     change_real_rank(ranks[idx], rank_item["real_rank"])
         return [v["real_rank"] for k, v in ranks.items()]
     else:
-        print("No significant differences found between the groups.")
+        if vis:
+            print("No significant differences found between the groups.")
         return []
 
 
@@ -75,14 +78,15 @@ def get_com(step2_data, r=2, alpha=0.8, columns=None, vis=True):
             if max_rank_count < len(final_ranks) / 2:
                 for index, (test_acc_name, rank) in enumerate(zip(test_acc_names, final_ranks)):
                     if rank == max_rank:
-                        # print(f"index={index},name={test_acc_name},rank={rank}")
+                        if vis:
+                            print(f"index={index},name={test_acc_name},rank={rank}")
                         ans.append(test_acc_name if isinstance(test_acc_name, tuple) else (test_acc_name, ))
     return ans
 
 
 def get_frequent_itemsets(step2_data, threshold_per=0.1):
     threshold = int(len(step2_data) * threshold_per)
-    df_sorted = step2_data.sort_values(metric_name, ascending=False)
+    df_sorted = step2_data.sort_values(metric_name, ascending=ascending)
     top_10_percent = df_sorted.head(threshold)
     columns = sorted([col for col in step2_data.columns if col.startswith("pipeline")])
     transactions = top_10_percent[columns].values.tolist()
@@ -95,18 +99,18 @@ def get_frequent_itemsets(step2_data, threshold_per=0.1):
     return [tuple(a) for a in frequent_itemsets["itemsets"]]
 
 
-def get_com_all(step2_data, vis=True):
+def get_com_all(step2_data, vis=True, alpha=0.8):
     ans = []
     columns = sorted([col for col in step2_data.columns if col.startswith("pipeline")])
     for i in range(1, len(columns)):
-        ans += get_com(step2_data, i, columns=columns, vis=vis)
+        ans += get_com(step2_data, i, columns=columns, vis=vis, alpha=alpha)
     return ans
 
 
-def summary_pattern(data_path):
+def summary_pattern(data_path, alpha=0.8, vis=False):
     step2_origin_data = pd.read_csv(data_path)
     step2_data = step2_origin_data.dropna()
-    com_ans = get_com_all(step2_data, vis=False)
+    com_ans = get_com_all(step2_data, vis=vis, alpha=alpha)
     apr_ans = get_frequent_itemsets(step2_data)
     return list(set(com_ans) & set(apr_ans))
 
@@ -120,14 +124,23 @@ def summary_pattern(data_path):
 #                 algorithm,dataset=file_path.relative_to(directory).parts[:2]
 #                 ans.append({"algorithm":algorithm,"dataset":dataset,"summary_pattern":summary_pattern(file_path)})
 #     pd.DataFrame(ans).to_csv(save_path)
-def list_files(directory, file_name="best_test_acc.csv"):
-    path = Path(directory)
-    for file_path in path.rglob('*'):
-        if file_path.is_file():
-            if file_path.name == file_name:
-                with open(Path(file_path.parent.resolve(), "pipeline_summary_pattern.txt"), 'w') as f:
-                    f.write(str(summary_pattern(file_path)))
+def list_files(directories, file_name="best_test_acc.csv", alpha=0.8, vis=False):
+    for directory in directories:
+        path = Path(directory)
+        for file_path in path.rglob('*'):
+            if file_path.is_file():
+                if file_path.name == file_name:
+                    print(file_path)
+                    with open(Path(file_path.parent.resolve(), "pipeline_summary_pattern.txt"), 'w') as f:
+                        f.write(str(summary_pattern(file_path, alpha=alpha, vis=vis)))
 
 
 if __name__ == "__main__":
-    list_files('/home/zyxing/dance/examples/tuning')
+    directories = []
+    for path in Path('/home/zyxing/dance/examples/tuning').iterdir():
+        if path.is_dir():
+            if str(path.name).startswith("cluster"):
+                directories.append(path)
+    list_files(directories)
+
+    # print(summary_pattern("/home/zyxing/dance/examples/tuning/cta_scdeepsort/328_138/results/pipeline/best_test_acc.csv",alpha=0.3,vis=True))
