73ba678e7bc0166ede488f8980e14f82c3b5d12c
xzy <xzy12386@163.com>
2024-04-17 09:54:04 +0800

    minor

--- examples/tuning/imputation_deepimpute/main.py
+++ examples/tuning/imputation_deepimpute/main.py
@@ -1,11 +1,12 @@
 import argparse
+import os
 import sys
 from pathlib import Path
 
 import numpy as np
 import torch
-import wandb
 
+import wandb
 from dance import logger
 from dance.datasets.singlemodality import ImputationDataset
 from dance.modules.single_modality.imputation.deepimpute import DeepImpute
@@ -45,6 +46,7 @@ if __name__ == '__main__':
     file_root_path = Path(params.root_path, params.dataset).resolve()
     logger.info(f"\n files is saved in {file_root_path}")
     pipeline_planer = PipelinePlaner.from_config_file(f"{file_root_path}/{params.tune_mode}_tuning_config.yaml")
+    os.environ["WANDB_AGENT_MAX_INITIAL_FAILURES"] = "2000"
 
     def evaluate_pipeline(tune_mode=params.tune_mode, pipeline_planer=pipeline_planer):
         wandb.init(settings=wandb.Settings(start_method='thread'))
