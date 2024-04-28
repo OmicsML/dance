c20bc1749029120fe11b3c668290370fc404058d
xzy <xzy12386@163.com>
2024-04-28 21:54:56 +0800

    add spagcn

---
+++ examples/tuning/domain_spagcn/main.py
@@ -0,0 +1,97 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+
+import wandb
+from dance.datasets.spatial import SpatialLIBDDataset
+from dance.modules.spatial.spatial_domain.spagcn import SpaGCN, refine
+from dance.pipeline import PipelinePlaner, get_step3_yaml, run_step3, save_summary_data
+from dance.utils import set_seed
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cache", action="store_true", help="Cache processed data.")
+    parser.add_argument("--sample_number", type=str, default="151673",
+                        help="12 human dorsolateral prefrontal cortex datasets for the spatial domain task.")
+    parser.add_argument("--beta", type=int, default=49, help="")
+    parser.add_argument("--alpha", type=int, default=1, help="")
+    parser.add_argument("--p", type=float, default=0.05,
+                        help="percentage of total expression contributed by neighborhoods.")
+    parser.add_argument("--l", type=float, default=0.5, help="the parameter to control percentage p.")
+    parser.add_argument("--start", type=float, default=0.01, help="starting value for searching l.")
+    parser.add_argument("--end", type=float, default=1000, help="ending value for searching l.")
+    parser.add_argument("--tol", type=float, default=5e-3, help="tolerant value for searching l.")
+    parser.add_argument("--max_run", type=int, default=200, help="max runs.")
+    parser.add_argument("--epochs", type=int, default=200, help="Number of epochs.")
+    parser.add_argument("--n_clusters", type=int, default=7, help="the number of clusters")
+    parser.add_argument("--step", type=float, default=0.1, help="")
+    parser.add_argument("--lr", type=float, default=0.05, help="learning rate")
+    parser.add_argument("--device", default="cpu", help="Computation device.")
+    parser.add_argument("--seed", type=int, default=100, help="")
+    parser.add_argument("--tune_mode", default="pipeline_params", choices=["pipeline", "params", "pipeline_params"])
+    parser.add_argument("--count", type=int, default=2)
+    parser.add_argument("--sweep_id", type=str, default=None)
+    parser.add_argument("--summary_file_path", default="results/pipeline/best_test_acc.csv", type=str)
+    parser.add_argument("--root_path", default=str(Path(__file__).resolve().parent), type=str)
+    parser.add_argument("--data_dir", type=str, default='../temp_data', help='test directory')
+    os.environ["WANDB_AGENT_MAX_INITIAL_FAILURES"] = "2000"
+    args = parser.parse_args()
+    file_root_path = Path(args.root_path, args.sample_number).resolve()
+    pipeline_planer = PipelinePlaner.from_config_file(f"{file_root_path}/{args.tune_mode}_tuning_config.yaml")
+
+    def evaluate_pipeline(tune_mode=args.tune_mode, pipeline_planer=pipeline_planer):
+        wandb.init(settings=wandb.Settings(start_method='thread'))
+        set_seed(args.seed)
+
+        # Initialize model and get model specific preprocessing pipeline
+        model = SpaGCN(device=args.device)
+        preprocessing_pipeline = model.preprocessing_pipeline(alpha=args.alpha, beta=args.beta)
+
+        # Load data and perform necessary preprocessing
+        dataloader = SpatialLIBDDataset(data_id=args.sample_number, data_dir=args.data_dir)
+        data = dataloader.load_data()
+        # Prepare preprocessing pipeline and apply it to data
+        kwargs = {tune_mode: dict(wandb.config)}
+        preprocessing_pipeline = pipeline_planer.generate(**kwargs)
+        print(f"Pipeline config:\n{preprocessing_pipeline.to_yaml()}")
+        preprocessing_pipeline(data)
+        (x, adj, adj_2d), y = data.get_train_data()
+
+        # Train and evaluate model
+        l = model.search_l(args.p, adj, start=args.start, end=args.end, tol=args.tol, max_run=args.max_run)
+        model.set_l(l)
+        res = model.search_set_res((x, adj), l=l, target_num=args.n_clusters, start=0.4, step=args.step, tol=args.tol,
+                                   lr=args.lr, epochs=args.epochs, max_run=args.max_run)
+
+        pred = model.fit_predict((x, adj), init_spa=True, init="louvain", tol=args.tol, lr=args.lr, epochs=args.epochs,
+                                 res=res)
+        score = model.default_score_func(y, pred)
+
+        refined_pred = refine(sample_id=data.data.obs_names.tolist(), pred=pred.tolist(), dis=adj_2d, shape="hexagon")
+        score_refined = model.default_score_func(y, refined_pred)
+        wandb.log({"ARI": score, "ARI (refined)": score_refined})
+
+    entity, project, sweep_id = pipeline_planer.wandb_sweep_agent(
+        evaluate_pipeline, sweep_id=args.sweep_id, count=args.count)  #Score can be recorded for each epoch
+    save_summary_data(entity, project, sweep_id, summary_file_path=args.summary_file_path, root_path=file_root_path)
+    if args.tune_mode == "pipeline" or args.tune_mode == "pipeline_params":
+        get_step3_yaml(result_load_path=f"{args.summary_file_path}", step2_pipeline_planer=pipeline_planer,
+                       conf_load_path=f"{Path(args.root_path).resolve().parent}/step3_default_params.yaml",
+                       root_path=file_root_path, required_funs=["SpaGCNGraph", "SpaGCNGraph2D", "SetConfig"],
+                       required_indexes=[5, 6, sys.maxsize], metric="ARI")
+        if args.tune_mode == "pipeline_params":
+            run_step3(file_root_path, evaluate_pipeline, tune_mode="params", step2_pipeline_planer=pipeline_planer)
+""" To reproduce SpaGCN on other samples, please refer to command lines belows:
+
+human dorsolateral prefrontal cortex sample 151673:
+$ python spagcn.py --sample_number 151673 --lr 0.1
+
+human dorsolateral prefrontal cortex sample 151676:
+$ python spagcn.py --sample_number 151676 --lr 0.02
+
+human dorsolateral prefrontal cortex sample 151507:
+$ python spagcn.py --sample_number 151507 --lr 0.009
+"""
