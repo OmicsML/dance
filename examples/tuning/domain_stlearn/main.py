2a634254d1bd2e04439da988adaa159138b9b68f
xzy <xzy12386@163.com>
2024-04-29 20:37:25 +0800

    add stlearn

---
+++ examples/tuning/domain_stlearn/main.py
@@ -0,0 +1,86 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+
+import wandb
+from dance.datasets.spatial import SpatialLIBDDataset
+from dance.modules.spatial.spatial_domain.stlearn import StKmeans, StLouvain
+from dance.pipeline import PipelinePlaner, get_step3_yaml, run_step3, save_summary_data
+from dance.utils import set_seed
+
+MODES = ["louvain", "kmeans"]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cache", action="store_true", help="Cache processed data.")
+    parser.add_argument("--sample_number", type=str, default="151673",
+                        help="12 human dorsolateral prefrontal cortex datasets for the spatial domain task.")
+    parser.add_argument("--mode", type=str, default="louvain", choices=MODES)
+    parser.add_argument("--n_clusters", type=int, default=17, help="the number of clusters")
+    parser.add_argument("--n_components", type=int, default=50, help="the number of components in PCA")
+    parser.add_argument("--device", type=str, default="cuda", help="device for resnet extract feature")
+    parser.add_argument("--seed", type=int, default=2)
+    parser.add_argument("--tune_mode", default="pipeline_params", choices=["pipeline", "params", "pipeline_params"])
+    parser.add_argument("--count", type=int, default=2)
+    parser.add_argument("--sweep_id", type=str, default=None)
+    parser.add_argument("--summary_file_path", default="results/pipeline/best_test_acc.csv", type=str)
+    parser.add_argument("--root_path", default=str(Path(__file__).resolve().parent), type=str)
+    parser.add_argument("--data_dir", type=str, default='../temp_data', help='test directory')
+    os.environ["WANDB_AGENT_MAX_INITIAL_FAILURES"] = "2000"
+    args = parser.parse_args()
+    file_root_path = Path(args.root_path, args.sample_number).resolve()
+    pipeline_planer = PipelinePlaner.from_config_file(f"{file_root_path}/{args.tune_mode}_tuning_config.yaml")
+
+    def evaluate_pipeline(tune_mode=args.tune_mode, pipeline_planer=pipeline_planer):
+        wandb.init(settings=wandb.Settings(start_method='thread'))
+        set_seed(args.seed)
+
+        # Initialize model and get model specific preprocessing pipeline
+        if args.mode == "kmeans":
+            model = StKmeans(n_clusters=args.n_clusters)
+        elif args.mode == "louvain":
+            model = StLouvain(resolution=0.6)
+        else:
+            raise ValueError(f"Unknown mode {args.mode!r}, available options are {MODES}")
+        # preprocessing_pipeline = model.preprocessing_pipeline(device=args.device)
+
+        # Load data and perform necessary preprocessing
+        dataloader = SpatialLIBDDataset(data_id=args.sample_number)
+        data = dataloader.load_data(cache=args.cache)
+        # Prepare preprocessing pipeline and apply it to data
+        kwargs = {tune_mode: dict(wandb.config)}
+        preprocessing_pipeline = pipeline_planer.generate(**kwargs)
+        print(f"Pipeline config:\n{preprocessing_pipeline.to_yaml()}")
+        preprocessing_pipeline(data)
+        x, y = data.get_data(return_type="default")
+
+        # Train and evaluate model
+        score = model.fit_score(x, y.values.ravel())
+        wandb.log({"ARI": score})
+
+    entity, project, sweep_id = pipeline_planer.wandb_sweep_agent(
+        evaluate_pipeline, sweep_id=args.sweep_id, count=args.count)  #Score can be recorded for each epoch
+    save_summary_data(entity, project, sweep_id, summary_file_path=args.summary_file_path, root_path=file_root_path)
+    if args.tune_mode == "pipeline" or args.tune_mode == "pipeline_params":
+        get_step3_yaml(result_load_path=f"{args.summary_file_path}", step2_pipeline_planer=pipeline_planer,
+                       conf_load_path=f"{Path(args.root_path).resolve().parent}/step3_default_params.yaml",
+                       root_path=file_root_path, required_funs=["StagateGraph", "SetConfig"],
+                       required_indexes=[5, sys.maxsize], metric="ARI")
+        if args.tune_mode == "pipeline_params":
+            run_step3(file_root_path, evaluate_pipeline, tune_mode="params", step2_pipeline_planer=pipeline_planer)
+""" To reproduce stlearn on other samples, please refer to command lines belows:
+NOTE: since the stlearn method is unstable, you have to run multiple times to get
+      best performance.
+
+human dorsolateral prefrontal cortex sample 151673:
+$ python stlearn.py --n_clusters 20 --sample_number 151673
+
+human dorsolateral prefrontal cortex sample 151676:
+$ python stlearn.py --n_clusters 20 --sample_number 151676
+
+human dorsolateral prefrontal cortex sample 151507:
+$ python stlearn.py --n_clusters 20 --sample_number 151507
+"""
