33bf424b4b681700b25f90a535cd2c8027244a7a
xingzhongyu <xzy12386@163.com>
2025-05-13 19:59:38 +0800

    minor

--- examples/tuning/cta_scdeepsort/main.py
+++ examples/tuning/cta_scdeepsort/main.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
     parser.add_argument("--summary_file_path", default="results/pipeline/best_test_acc.csv", type=str)
     parser.add_argument("--root_path", default=str(Path(__file__).resolve().parent), type=str)
     parser.add_argument("--filetype", default="csv")
+    parser.add_argument('--additional_sweep_ids', action='append', type=str, help='get prior runs')
     args = parser.parse_args()
     logger.setLevel(args.log_level)
     os.environ["WANDB_AGENT_MAX_INITIAL_FAILURES"] = "2000"
@@ -107,7 +108,7 @@ if __name__ == "__main__":
 
     entity, project, sweep_id = pipeline_planer.wandb_sweep_agent(
         evaluate_pipeline, sweep_id=args.sweep_id, count=args.count)  #Score can be recorded for each epoch
-    save_summary_data(entity, project, sweep_id, summary_file_path=args.summary_file_path, root_path=file_root_path)
+    save_summary_data(entity, project, sweep_id, summary_file_path=args.summary_file_path, root_path=file_root_path,additional_sweep_ids=args.additional_sweep_ids)
     if args.tune_mode == "pipeline" or args.tune_mode == "pipeline_params":
         get_step3_yaml(
             result_load_path=f"{args.summary_file_path}",
